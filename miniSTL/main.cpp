8a564581607df4c46aed99076bbdbb053229fdef
dogrest <dogrest@dogrestdeMacBook-Pro.local>
2015-09-15 20:01:21 +0800

    Initial Commit

---
+++ miniSTL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  miniSTL
+//
+//  Created by dogrest on 15/9/15.
+//  Copyright (c) 2015年 dogrest. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
